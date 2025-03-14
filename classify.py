ad975de89ec7d40aa0d219facc29665fa835df40
ripaa <90957953+rifaaa12@users.noreply.github.com>
2025-03-15 02:15:34 +0700

    Update classify.py
    adding elapsed time

--- classify.py
+++ classify.py
@@ -4,10 +4,12 @@ from download_models import download_all_models, print_results_in_percentage, _i
 from huggingface_hub import HfFileSystem
 import shutil
 from get_task import get_task
+import time
 
 hfs = HfFileSystem()
 
 def run_classification(task_name, image_path, model_name=None, imgsize=384):
+    start_time = time.time()
     print(f"Starting classification task '{task_name}' on image '{image_path}'")
     
     task = get_task(task_name)
@@ -26,6 +28,11 @@ def run_classification(task_name, image_path, model_name=None, imgsize=384):
     
     print("Classification complete. Printing results:")
     print_results_in_percentage(result)
+    end_time = time.time()  # End timing for all images
+    elapsed_time = end_time - start_time  # Calculate total elapsed time
+    
+    summary_message = f"Total classification time: {elapsed_time:.2f} seconds.\n"
+    print(summary_message)
 
 
 
@@ -36,6 +43,8 @@ def classify_images_in_folder(task_name, image_folder, output_folder, model_name
     os.makedirs(output_folder, exist_ok=True)
     
     log_file = os.path.join(output_folder, "classification_log.txt")
+    
+    start_time = time.time()  # Start timing for all images
     
     with open(log_file, "w") as log:
         for filename in os.listdir(image_folder):
@@ -54,7 +63,7 @@ def classify_images_in_folder(task_name, image_folder, output_folder, model_name
                     
                     highest_label = max(result, key=result.get)
                     highest_percentage = result[highest_label]
-                    message = f"Image '{filename}' classified as '{highest_label}' with {highest_percentage:.2f}% confidence.\n"
+                    message = (f"Image '{filename}' classified as '{highest_label}' with {highest_percentage:.2f}% confidence.\n")
                     print(message)
                     log.write(message)
                     
@@ -72,4 +81,12 @@ def classify_images_in_folder(task_name, image_folder, output_folder, model_name
                     message = f"Skipping corrupted image: {filename}. Error: {str(e)}\n"
                     print(message)
                     log.write(message)
+    
+    end_time = time.time()  # End timing for all images
+    elapsed_time = end_time - start_time  # Calculate total elapsed time
+    
+    summary_message = f"Total classification time: {elapsed_time:.2f} seconds.\n"
+    print(summary_message)
+    with open(log_file, "a") as log:
+        log.write(summary_message)
 
